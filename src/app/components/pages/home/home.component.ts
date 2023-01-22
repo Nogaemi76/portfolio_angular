78cdeb6a92b333d400c497dc5cb9c3f669ff8093
Emilie <milogare@yahoo.fr>
2023-01-22 20:19:01 +0100

    Added "open pdf link"

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  resume_url: string = 'assets/documents/cv_en_devfront.pdf';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openResumePdf() {
+    window.open(this.resume_url, '_blank');
   }
 
 }
